7ca291451868a640e270c55ca827183edd34b8f6
ford burgess <fburg6824@gmail.com>
2022-02-22 16:30:43 +0000

    added image attribute to venue model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_113440) do
+ActiveRecord::Schema.define(version: 2022_02_22_161336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2022_02_22_113440) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
