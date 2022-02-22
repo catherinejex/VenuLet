c7b1c77d65f23f9e11704dc36eba670cd058165e
Minja Slavkovic <minja.slavkovic@gmail.com>
2022-02-22 16:38:56 +0000

    finished index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 30.times do
+#   user = User.new(email: "bob@bob.gmail")
+#   user.save
+# end
+
+30.times do
+  venue = Venue.new(title: ['Cozy Chalet', 'Magnificent Castle', 'Stately Manor'].sample,
+              location: ['Vail, Colorado', 'Lyon, France', ].sample,
+              description: ['Warm and cozy chalet, nice views, great for couples',
+              'Splendid and spacious castle, very regal, great large parties',
+              'Comfortable 19th century manor, fully furnished, perfect for weekend getaways'].sample,
+              rate: [250, 999, 499].sample, square_meters: [100, 250, 500].sample,
+              user_id: 1)
+  venue.save
+end
+puts 'Seed finished'
